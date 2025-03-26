87dcb240e684d488c17ea4cb5f684b12549beb62
dusanlazicaerospace <dusanjovicalazic@outlook.com>
2025-03-26 11:53:13 +0100

    Second Commint - Added settings.py for bg colour, screen width and screen height

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,19 +1,25 @@
 import sys
-
+from settings import Settings
 import pygame
 
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-        self.screen=pygame.display.set_mode((1600,800)) # Resolution of the main window 
+        self.clock=pygame.time.Clock() # Initilize game clock
+        self.settings=Settings()
+        self.screen=pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height)) # Using the numbers from class settings.py
         pygame.display.set_caption("Alien Invasion") # Top title on the top bar
+        
+
     def run_game(self):
         while True: # Running the game
             for event in pygame.event.get(): # Get events
                 if event.type == pygame.QUIT: # If we the event is quit
-                    sys.exit() #Exit the game using the sys package 
-        pygame.display.flip()
+                    sys.exit() #Exit the game using the sys package
+            self.screen.fill(self.settings.screen_bg_colour) #Using the numbers from settings.py for the colour  
+            pygame.display.flip() 
+            self.clock.tick(60) # Ajust the frame rate (60 frames per second)
 
 if __name__=="__main__":
     ai=AlienInvasion()
---
+++ settings.py
@@ -0,0 +1,5 @@
+class Settings: 
+    def __init__ (self):
+        self.screen_width=1600
+        self.screen_height=800
+        self.screen_bg_colour=(230,230,230)
