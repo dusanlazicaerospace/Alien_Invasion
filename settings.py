d395dfe1bbdfeeaaf9f5cb851a74b2a12b5086f0
dusanlazicaerospace <dusanjovicalazic@outlook.com>
2025-03-26 15:00:15 +0100

    6th Commit - Added continus movment left and right in a module/attribute and changed gamespeed to 1.5

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 class Settings: 
     def __init__ (self):
+        
         self.screen_width=1600
         self.screen_height=800
         self.screen_bg_colour=(230,230,230)
+        self.ship_speed=1.5
--- ship.py
+++ ship.py
@@ -4,6 +4,7 @@ class Ship:
     def __init__(self,ai_game):
         #Initilise the ship and get to its possition
         self.screen=ai_game.screen
+        self.settings=ai_game.settings
         self.screen_rect=ai_game.screen.get_rect()
 
         #Load the ship image and get its position
@@ -12,16 +13,20 @@ class Ship:
         
         #Start each new ship on the bottom middle of the screen
         self.rect.midbottom=self.screen_rect.midbottom
-    
+
+        #Stroe a float for the ships exact horizontal position 
+        self.x=float(self.rect.x)
+
         # Movement flag; start with a ship that is not moving
         self.moving_right=False
         self.moving_left= False
 
     def update(self):# Update the ships position based on the movment flag 
         if self.moving_right:
-            self.rect.x+= 1 
+            self.x+= self.settings.ship_speed
         if self.moving_left:
-            self.rect.x-= 1
+            self.x-= self.settings.ship_speed
+        self.rect.x = self.x
     
     def blitme(self):
         self.screen.blit(self.image,self.rect)
